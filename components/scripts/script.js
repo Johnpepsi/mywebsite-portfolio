5ee07cdad1362a5add86348f45aa940c3f3b52ef
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2025-01-27 01:36:55 -0600

    Update script.js

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -157,7 +157,7 @@ document.getElementById("contact-form").onsubmit = function(e) {
 
 // ========== Configuring typing animation ===========
 var typed = new Typed(".auto-type", {
-    strings: ['i0s Mobile Developer', 'Full-Stack Web Developer'],
+    strings: ['IT Administrator', 'Full-Stack Web Developer', 'Tech Enthusiast'],
     typeSpeed: 60,
     backSpeed: 80,
     loop: true,
