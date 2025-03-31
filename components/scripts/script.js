24073114152d19a6648ae98cade283dee79816a5
John Palaganas <palaganasjohnashley@gmail.com>
2025-03-31 13:45:36 -0600

    commit

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -157,7 +157,7 @@ document.getElementById("contact-form").onsubmit = function(e) {
 
 // ========== Configuring typing animation ===========
 var typed = new Typed(".auto-type", {
-    strings: ['IT Administrator', 'Tech Enthusiast'],
+    strings: ['System Support', 'IT Administrator', 'Front-End Developer'],
     typeSpeed: 60,
     backSpeed: 80,
     loop: true,
